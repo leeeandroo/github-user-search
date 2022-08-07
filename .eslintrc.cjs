74db9a2b540210279cd2c261119aa3f65e7c40dc
Leandro <leeeandroo@gmail.com>
2022-08-07 13:34:23 +0200

    chore: Add JSX.Element type and no-undef off (#2)

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -46,4 +46,12 @@ module.exports = {
       },
     ],
   },
+  overrides: [
+    {
+      files: ['*.ts', '*.mts', '*.cts', '*.tsx'],
+      rules: {
+        'no-undef': 'off',
+      },
+    },
+  ],
 };
