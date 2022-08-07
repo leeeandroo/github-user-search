e6e41a0455861a230316b45adc0b15caeec22d52
leeeandroo <leeeandroo@gmail.com>
2022-08-07 13:24:23 +0200

    chore: Some test setup improvements
    Move setup and utils to tests folder.

--- src/App.spec.tsx
+++ src/App.spec.tsx
@@ -1,4 +1,4 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen } from '../tests/utils';
 import { it, expect } from 'vitest';
 import App from './App';
 
--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,6 @@ export default defineConfig({
   test: {
     globals: true,
     environment: 'jsdom',
-    setupFiles: './setupTests.ts',
+    setupFiles: './tests/setup.ts',
   },
 });
